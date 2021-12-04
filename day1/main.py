9574199f40f712928e161c5815f909987e920df8
c2dan88 <wildteen88@googlemail.com>
2021-12-04 08:01:21 +0000

    day1 part2

--- day1/main.py
+++ day1/main.py
@@ -1,19 +1,26 @@
 
-def part_1(lines):
-    prev_depth = lines[0]
-    depth_increases = 0
-    for line in lines[1:]:
-        if line > prev_depth:
-            depth_increases += 1
+def part_1(depths):
+    prev_depth = depths[0]
+    total = 0
+    for depth in depths[1:]:
+        if depth > prev_depth:
+            total += 1
             #print(f"{line} (increased)")
-        prev_depth = line
-    print(depth_increases)
+        prev_depth = depth
+    return total
+
+
+def part_2(lines):
+    n = 3  # window size
+    window_depths = [sum(lines[i:i + n]) for i in range(0, len(lines))]
+    return part_1(window_depths)
 
 
 if __name__ == "__main__":
     f = open('puzzle.txt', 'r+')
     lines = [int(line) for line in f.readlines()]
 
-    part_1(lines)
+    print("Part 1:", part_1(lines))
+    print("Part 2:", part_2(lines))
 
 
